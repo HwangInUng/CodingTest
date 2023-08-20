37f1fdfccaa58edb170539625a6622a77f3c8ea0
unknown <ung6860@naver.com>
2023-08-20 23:04:04 +0900

    백준 구간 합 구하기 2 복습 -세그먼트 트리 선언 및 이해 -mid = start - end / 2에서 계속 오류가 발생 -mid = start + end / 2로ã 작성해야함.

--- src/test/level2/구간합2_1.java
+++ src/test/level2/구간합2_1.java
@@ -1,95 +1,97 @@
 package test.level2;
 
-import javax.swing.text.Segment;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class 구간합2_1 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
-
-        int n = Integer.valueOf(stringTokenizer.nextToken());
-        int m = Integer.valueOf(stringTokenizer.nextToken());
-        int k = Integer.valueOf(stringTokenizer.nextToken());
+        int n = Integer.valueOf(st.nextToken());
+        int m = Integer.valueOf(st.nextToken());
+        int k = Integer.valueOf(st.nextToken());
 
         long[] arr = new long[n + 1];
-
-        for (int i = 1; i <= n; i++){
-            arr[i] = Long.valueOf(bufferedReader.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Long.valueOf(br.readLine());
         }
 
-        SegmentTree segmentTree = new SegmentTree(arr);
-        segmentTree.init(1, 1, n);
+        SegmentTree tree = new SegmentTree(arr);
+        tree.init(1, 1, n);
 
-        for(int i = 0; i < m+k; i++){
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
-            int a = Integer.valueOf(stringTokenizer.nextToken());
-            int b = Integer.valueOf(stringTokenizer.nextToken());
-            long c = Long.valueOf(stringTokenizer.nextToken());
+        for (int i = 0; i < m + k; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
 
-            if (a == 1){
-                segmentTree.update(1, 1, n, b, c - segmentTree.getnArray()[b]);
-                segmentTree.updatenArray(b, c);
-            } else{
-                System.out.println(segmentTree.sum(1, 1, n, b, (int)c));
+            int a = Integer.valueOf(st.nextToken());
+            int b = Integer.valueOf(st.nextToken());
+            long c = Long.valueOf(st.nextToken());
+
+            if (a == 1) {
+                tree.update(1, 1, n, b, c - tree.getArray()[b]);
+                tree.updateArray(b, c);
+            } else {
+                System.out.println(tree.sum(1, 1, n, b, (int) c));
             }
         }
-        bufferedReader.close();
+        br.close();
     }
 
-   static class SegmentTree{
-        private long[] tree;
+    private static class SegmentTree {
         private long[] nArray;
+        private long[] tree;
 
-        public SegmentTree(long[] arr){
-            int height = (int) Math.ceil(Math.log(arr.length - 1) / Math.log((2)));
+        public SegmentTree(long[] arr) {
+            int height = (int) Math.ceil(Math.log(arr.length - 1) / Math.log(2));
+            int treeSize = (int) Math.pow(2, height + 1);
 
-            tree = new long[(int)Math.pow(2, height + 1)];
+            tree = new long[treeSize];
             this.nArray = arr;
         }
 
-        public long init(int node, int start, int end){
-            if (start == end){
+        public long init(int node, int start, int end) {
+            if (start == end) {
                 return this.tree[node] = this.nArray[start];
             }
-            return this.tree[node] = init(node * 2, start, (start + end) / 2) +
-                    init(node * 2 + 1, (start + end) / 2 + 1, end);
+
+            int mid = (start + end) / 2;
+            return this.tree[node] = init(node * 2, start, mid) + init(node * 2 + 1, mid + 1, end);
         }
 
-        public void update(int node, int start, int end, int index, long diff){
-            if (index < start || end < index){
+        public void update(int node, int start, int end, int index, long diff) {
+            if (index < start || end < index) {
                 return;
             }
 
             this.tree[node] += diff;
+            int mid = (start + end) / 2;
 
-            if(start != end){
-                update(node * 2, start, (start + end) / 2, index, diff);
-                update(node * 2 + 1, (start + end) / 2 + 1, end, index, diff);
+            if (start != end) {
+                update(node * 2, start, mid, index, diff);
+                update(node * 2 + 1, mid + 1, end, index, diff);
             }
         }
 
-        public long sum(int node, int start, int end, int left, int right){
-            if(left > end || right < start){
+        public long sum(int node, int start, int end, int left, int right) {
+            if (left > end || right < start) {
                 return 0;
             }
 
-            if(left <= start && end <= right){
+            if (left <= start && end <= right) {
                 return this.tree[node];
             }
 
-            return sum(node * 2, start, (start + end) / 2, left, right)
-                    + sum(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+            int mid = (start + end) / 2;
+            return sum(node * 2, start, mid, left, right) +
+                    sum(node * 2 + 1, mid + 1, end, left, right);
         }
 
-        public long[] getnArray(){
+        public long[] getArray() {
             return this.nArray;
         }
 
-        public void updatenArray(int index, long value){
+        public void updateArray(int index, long value) {
             this.nArray[index] = value;
         }
     }
