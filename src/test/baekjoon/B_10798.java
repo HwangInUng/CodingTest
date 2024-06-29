55d515f7b3000ad650b90a8bee0df2dc61ea471c
HwangInUng <ung6860@naver.com>
2024-06-30 00:51:38 +0900

    baekjoon 10798 - 2차원 배열의 각 행의 지정된 열 번호 읽기 - 1차원 배열의 각 길이가 다름 - 배열 내 가장 긴 길이를 기준으로 행의 길이보다 작다면 생략

---
+++ src/test/baekjoon/B_10798.java
@@ -0,0 +1,33 @@
+package test.baekjoon;
+
+import java.io.*;
+
+public class B_10798 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int n = 5;
+        int maxLength = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            sb.append(br.readLine());
+            if (i != n - 1) {
+                sb.append("\n");
+            }
+        }
+        String[] arr = sb.toString().split("\n");
+        for(String s : arr) {
+            maxLength = Math.max(maxLength, s.length());
+        }
+
+        for(int i = 0; i < maxLength; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i > arr[j].length() - 1) continue;
+                bw.write(arr[j].charAt(i));
+            }
+        }
+
+        bw.flush();
+    }
+}
