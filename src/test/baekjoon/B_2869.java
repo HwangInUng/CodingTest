7e8c7fb26cfe884f563056bd49b0fa0328f0b69d
HwangInUng <ung6860@naver.com>
2024-07-02 22:35:50 +0900

    baekjoon 2869 - 시간초과로 인한 코드 수정 - 목표에서 b를 빼서 목적지를 설정해야하는 부분 미스 - 추가로 반복문이 아닌 나머지 연산을 통해 해결 가능한 문제였음

---
+++ src/test/baekjoon/B_2869.java
@@ -0,0 +1,32 @@
+package test.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_2869 {
+    public static void main(String[] args) throws IOException {
+        /*
+         * 하루에 올라갈 수 있는 미터 : A - B
+         * 하루가 끝난 시간 기준으로 A를 먼저 더 해보고 조건판단
+         * 만약, A == V 같다면 하루만에 완등 가능
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        int dayDist = a - b;
+        int goalDist = v - b; // 도달한 순간에는 b를 빼면안되기 때문에 v-b를 목적지로 선정
+
+        if(goalDist % dayDist != 0){
+            // 나머지가 있는 경우 하루를 더 추가해야함
+            System.out.println(goalDist/dayDist + 1);
+        } else {
+            System.out.println(goalDist/dayDist);
+        }
+    }
+}
