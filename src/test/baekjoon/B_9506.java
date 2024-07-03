6c094ebe369bca830ceeee3ca5363b396c8e5dc3
HwangInUng <ung6860@naver.com>
2024-07-03 22:07:32 +0900

    baekjoon 9506 - 약수들의 합 구하기 - n의 약수 중 n제외한 모든 약수의 합이 n인지 판별 - 즉, 완전수 인지 확인 - 1은 소수, 완전수가 아닌 유일한 자연수

---
+++ src/test/baekjoon/B_9506.java
@@ -0,0 +1,42 @@
+package test.baekjoon;
+
+import java.io.*;
+
+public class B_9506 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            int a = Integer.parseInt(br.readLine());
+            int sum = 0;
+            StringBuilder sb = new StringBuilder();
+
+            if (a == -1) break;
+
+            for (int i = 1; i <= a; i++) {
+                if (i == a) break;
+
+                if (a % i == 0) {
+                    sum += i;
+                    sb.append(i + " ");
+                }
+            }
+
+            String[] arr = sb.toString().split(" ");
+            sb.delete(0, sb.length());
+            for (int i = 0; i < arr.length; i++) {
+                int num = Integer.parseInt(arr[i]);
+                if (i != 0) {
+                    sb.append(" + ");
+                }
+                sb.append(num);
+            }
+
+            if (sum == a) bw.write(a + " = " + sb.toString() + "\n");
+            else bw.write(a + " is NOT perfect." + "\n");
+        }
+
+        bw.flush();
+    }
+}
