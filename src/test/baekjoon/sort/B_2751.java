097de2d7028de8a647ebae5102c86509c7871898
HwangInUng <ung6860@naver.com>
2024-07-07 09:39:05 +0900

    baekjoon 2751 - O(n2)로 해결 불가능 문제 - O(nlogn)의 알고리즘으로 해결 필요 - Arrays.sort()와 Collections.sort()의 차이를 확인

---
+++ src/test/baekjoon/sort/B_2751.java
@@ -0,0 +1,30 @@
+package test.baekjoon.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class B_2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            arr.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(arr);
+
+        for (int i : arr) {
+            sb.append(i).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
